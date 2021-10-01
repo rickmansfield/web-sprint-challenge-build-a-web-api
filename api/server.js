4f02d7642bb0aab0f6ffebec3d4840c5d72cf7ce
Rick <ricksmycodeguy@gmail.com>
2021-10-01 10:43:55 -0500

    added Dependencies, index.js and server.js

--- api/server.js
+++ api/server.js
@@ -1,9 +1,22 @@
-const express = require('express');
-const server = express();
+const express = require('express')
+const server = express()
+const projectsRouter = require('./projects/projects-router')
+const actionsRouter = require('./actions/actions-router')
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.json({ message: 'Api is working add a route to see more' })
+})
+
+server.use('/api/actions', actionsRouter)
+server.use('/api/projects', projectsRouter)
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
 
 module.exports = server;
